0b936fa3b0c165648ee4910ef31d52d60e0b9583
K.Bailey <katebailey1230@gmail.com>
2018-10-13 11:46:34 -0400

    sudoku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '63xh(!rajwpfyww(v50%1wf2wy+58koitm1ev&fa7)e4!*pyl#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
